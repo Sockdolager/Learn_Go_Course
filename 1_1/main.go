6ac2ce33d3680f1bfd1f650d9f305b19c7a758e1
Joshua Wilkins <joshdotwilkins@outlook.com>
2021-05-04 10:43:27 -0400

    Per Course Module

---
+++ 1_1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Sockdolager/Learn_Go_Course/mascot"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(mascot.BestMoscot())
+	fmt.Println(quote.Go())
+}
